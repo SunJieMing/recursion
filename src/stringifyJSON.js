a8f015b7ef0aed94b943c001bc0851c277b2e4d9
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-07-06 01:39:57 -0600

    I completed the stringifyJSON function.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,59 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringifiedObject = '';
+  var test = typeof obj;
+
+  if(obj === null){
+    stringifiedObject = "" + null;
+  }
+  else if(test === 'string'){
+    stringifiedObject = '' + '"' + obj + '"';
+  }
+  else if( Object.prototype.toString.call( obj ) === '[object Array]' ) {
+    if(obj.length){
+      stringifiedObject += '[';
+      for(var i = 0; i < obj.length; i++){
+        if(i !== 0){
+          stringifiedObject += ',';
+        }
+      stringifiedObject += stringifyJSON(obj[i]);
+      }
+      stringifiedObject += ']';
+    } else {
+      stringifiedObject = '[]';
+    }
+  }
+  else if(test === 'object'){
+    var stringifiedObject = '{';
+    var count = 0;
+    if(Object.getOwnPropertyNames(obj).length === 0){
+      stringifiedObject = '{}';
+    } else {
+      for(key in obj){
+        var valueTest = typeof obj[key];
+        if (valueTest === 'function' || obj[key] === undefined) continue;
+        if(count !== 0){
+          stringifiedObject += ',';   
+        }
+        stringifiedObject += '"' + key + '":';
+
+        var test = typeof obj[key];
+
+        if(test === 'object'){
+          stringifiedObject += stringifyJSON(obj[key]);
+        }
+        else if(test === 'string'){
+          stringifiedObject += '"' + obj[key] + '"';
+        }
+        else {
+          stringifiedObject += obj[key];   
+        }
+        count++;
+      } 
+      stringifiedObject += '}';
+    }
+  } else {
+    stringifiedObject = "" + obj;
+  }
+
+  return stringifiedObject;
 };
